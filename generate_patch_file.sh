177a1df2f5a425188c0ab786e35c5dadf72edf43
Wei Dong <wdong@mshsrv04.mshnisen>
2012-03-09 13:10:37 +0800

    Signed-off-by: Wei Dong <wdong@marvell.com>
    change for generate ref file and patch

--- generate_patch_file.sh
+++ generate_patch_file.sh
@@ -2,6 +2,7 @@
 sour=$1
 dest=$2
 output=$3
-cd ~/buildbot_script/
-. /home/buildfarm/buildbot_script/buildbot/genDiff.sh ${sour} ${dest} ${output}
-. /home/buildfarm/buildbot_script/buildbot/genDeltaPatch.sh ${sour} ${dest} ${output}
+cd ${output}
+cp /home/buildfarm/buildbot_script/buildbot/diffz ./
+. /home/buildfarm/buildbot_script/buildbot/genDiff.sh ${sour} ${dest}
+. /home/buildfarm/buildbot_script/buildbot/genDeltaPatch.sh ${sour} ${dest}
