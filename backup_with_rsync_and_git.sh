8488453d6f3e8b81859ec5c6efee6d82b578e38b
Yufei Shi <yfshi@marvell.com>
2012-12-10 10:10:58 +0800

    mars:add log print for the rsync backup script
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- backup_with_rsync_and_git.sh
+++ backup_with_rsync_and_git.sh
@@ -1,24 +1,28 @@
-#!/bin/sh
-
-SCRIPT=`readlink -f $0`
-BASEDIR=$SCRIPT
+CRIPT=`readlink -f $0`
+BASEDIR=$(dirname $SCRIPT)
 REMOTE_SERVER=github.marvell.com
 ADMIN_USER=buildfarm
 SRC_DIR=/mobile/android/default
-DEST_DIR=/home/github_backup
+DEST_DIR=/home/buildfarm/github_backup
+LOGFILE=$BASEDIR/logs/backup_github.log
 
-#cd $BASEDIR
+for remote_server in $REMOTE_SERVER; do
+        echo "" | tee -a $LOGFILE
+        echo "start at: `date --rfc-3339=seconds`" | tee -a $LOGFILE
 
-rsync -az --delete -e ssh $ADMIN_USER@$REMOTE_SERVER:$SRC_DIR $DEST_DIR
-# Add as many directory as you wish.
-# rsync -az --delete -e ssh $ADMIN_USER@$REMOTE_SERVER:$SRC_DIR $DEST_DIR
+        rsync -azv --delete -e ssh $ADMIN_USER@$REMOTE_SERVER:$SRC_DIR $DEST_DIR | tee -a $LOGFILE
+        # Add as many directory as you wish.
+        # rsync -az --delete -e ssh $ADMIN_USER@$REMOTE_SERVER:$SRC_DIR $DEST_DIR
+        echo "rsync done at: `date --rfc-3339=seconds`" | tee -a $LOGFILE
+        cd $DEST_DIR
 
-cd $DEST_DIR
+        if [ ! -d ".git" ]; then
+                git init . | tee -a $LOGFILE
+        fi
 
-if [ ! -d ".git" ]; then
-  git init .
-fi
+        git add -A | tee -a $LOGFILE
+        git commit -a -s -m "Autorsync:synced from $REMOTE_SERVER:$SRC_DIR `date --rfc-3339=seconds`" | tee -a $LOGFILE
+        git gc | tee -a $LOGFILE
 
-git add -A
-git commit -a -s -m "Autorsync:synced from $REMOTE_SERVER:$SRC_DIR `date --rfc-3339=seconds`"
-git gc
+        echo "done at: `date --rfc-3339=seconds`" | tee -a $LOGFILE
+done
