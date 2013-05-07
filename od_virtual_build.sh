c90488feae9188912386e7cc8e463127d2af0b5b
Yufei Shi <yfshi@marvell.com>
2013-05-07 18:35:15 +0800

    mars: add cd path for send mail
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- od_virtual_build.sh
+++ od_virtual_build.sh
@@ -3,4 +3,5 @@ set -o pipefail
 STD_LOG=/home/buildfarm/buildbot_script/stdio.log
 rm $STD_LOG
 . ~/buildbot_script/buildbot/core/check_update.sh | tee -a $STD_LOG
+cd ~/buildbot_script/buildbot
 . ~/buildbot_script/buildbot/dev/on_demand_virtual_build.sh $@ | tee -a $STD_LOG
