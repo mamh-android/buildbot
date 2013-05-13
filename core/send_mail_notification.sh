a5332d994d0010b11e1e8c11772f5032c592506b
Yufei Shi <yfshi@marvell.com>
2013-05-13 14:17:23 +0800

    core: enabled send mail for upload pub release build
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- core/send_mail_notification.sh
+++ core/send_mail_notification.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 GERRIT_PATCH=
 MANIFEST_XML=
 MANIFEST_BRANCH=
@@ -8,6 +7,9 @@ PLATFORM_ANDROID_VARIANT=
 ABS_BUILD_DEVICES=
 USEREMAIL=
 BUILDTYPE=
+BRANCH_NAME=
+TAG_NAME=
+GITACCESS_FILE=
 build_maintainer="yfshi@marvell.com"
 STD_LOG=/home/buildfarm/buildbot_script/stdio.log
 PACKAGE_LINK=$( awk -F"<result-dir>|</result-dir>" ' /<result-dir>/ { print $2 } ' $STD_LOG )
@@ -19,8 +21,8 @@ help() {
     echo
   fi
   echo "HELP!!!!"
-  echo "-e [useremail] -t [build type]"
-  echo "-e -t"
+  echo "-e [useremail] -t [build type] -b branch name[] -a [tag name]"
+  echo "-e -t -b -a"
   exit 1
 }
 
@@ -42,14 +44,25 @@ validate_parameters() {
           help "Please give a valid build type."
         fi
         BUILDTYPE=$2
-#        shift 1
-#        continue
+        ;;
+      "-b")
+        if [ -z "$2" ]; then
+          help "Please give a valid branch name."
+        fi
+        BRANCH_NAME=$2
+        ;;
+      "-a")
+        if [ -z "$2" ]; then
+          help "Please give a valid tag name."
+        fi
+        TAG_NAME=$2
         ;;
     esac
     shift 1
   done
 }
 
+#Generate mail
 generate_error_notification_email() {
 cat <<-EOF
 From: $build_maintainer
@@ -75,7 +88,7 @@ Team of $DEV_TEAM
 EOF
 }
 
-generate_success_notification_email() {
+generate_odvb_success_notification_email() {
 cat <<-EOF
 From: $build_maintainer
 To: $USEREMAIL
@@ -95,23 +108,69 @@ Team of $DEV_TEAM
 EOF
 }
 
+generate_uprb_success_notification_email() {
+cat <<-EOF
+From: $build_maintainer
+To: $USEREMAIL
+Subject: [$BUILDTYPE] is done.
+
+This is an automated email from the autobuild script. It was
+generated because your triggered $BUILDTYPE is success.
+
+.gitaccess file is attached. Please forward this mail and upload the .gitaccessfile
+
+Complete Time: $(date)
+Build Host: $(hostname)
+
+---
+Team of $DEV_TEAM
+EOF
+uuencode $GITACCESS_FILE ${GITACCESS_FILE##/*/}
+}
+
+#Send mail
 send_error_notification() {
   echo "generating error notification email"
   generate_error_notification_email  | /usr/sbin/sendmail -t $build_maintainer
 }
 
-send_success_notification() {
-  echo "generating success notification email"
-  generate_success_notification_email | /usr/sbin/sendmail -t $build_maintainer
+send_odvb_success_notification() {
+  echo "generating odvb success notification email"
+  generate_odvb_success_notification_email | /usr/sbin/sendmail -t $build_maintainer
+}
+
+send_uprb_success_notification() {
+  echo "generating uprb success notification email"
+  GITACCESS_FILE=$(./create_gitaccess.py -b $BRANCH_NAME -t $TAG_NAME)
+  echo "gitaccess file "$GITACCESS_FILE
+  generate_uprb_success_notification_email | /usr/sbin/sendmail -t $build_maintainer
 }
 
 validate_parameters $*
 
-result=`grep ">PASS<" $STD_LOG`
-if [ -n "$result" ]; then
-  send_success_notification
-  exit 0
-else
-  send_error_notification
-  exit 0
-fi
+case "$BUILDTYPE" in
+  "on_demand_virtual_build")
+    result=`grep ">PASS<" $STD_LOG`
+    if [ -n "$result" ]; then
+      send_odvb_success_notification
+      exit 0
+    else
+      send_error_notification
+      exit 0
+    fi
+  ;;
+  "upload_public_release_build")
+    result=`grep "upload finished!" $STD_LOG`
+    if [ -n "$result" ]; then
+      send_uprb_success_notification
+      exit 0
+    else
+      send_error_notification
+      exit 0
+    fi
+  ;;
+  *)
+    echo "$BUILDTYPE is not valid"
+    exit 1
+  ;;
+esac
--- upload_publish_release_build.sh
+++ upload_publish_release_build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-. ~/buildbot_script/buildbot/core/check_update.sh
-. ~/buildbot_script/buildbot/core/upload_pub_rls.sh $@
+set -o pipefail
+STD_LOG=/home/buildfarm/buildbot_script/stdio.log
+rm $STD_LOG
+. ~/buildbot_script/buildbot/core/check_update.sh | tee -a $STD_LOG
+cd ~/buildbot_script/buildbot
+. ~/buildbot_script/buildbot/core/upload_pub_rls.sh $@ | tee -a $STD_LOG
