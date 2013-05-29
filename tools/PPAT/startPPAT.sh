ab4939a20fecf5bd0f7c86bf42cbefac18bb46f3
Yufei Shi <yfshi@marvell.com>
2013-05-29 11:04:45 +0800

    tools: ppat add startPPAT script support
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- tools/PPAT/startPPAT.sh
+++ tools/PPAT/startPPAT.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-ssh zhoulz@10.38.32.178 /home/zhoulz/ppat/startPPAT.sh $@
+set -o pipefail
+STD_LOG=/home/buildfarm/buildbot_script/stdio.log
+PACKAGE_LINK=$( awk -F"<result-dir>http://10.38.116.40|</result-dir>" ' /<result-dir>/ { print $2 } ' $STD_LOG )
+IDIR="PROPERTY_DIR"
+
+result=`grep ">PASS<" $STD_LOG`
+if [ -n "$result" ]; then
+  a=${@//$IDIR/$PACKAGE_LINK}
+  ssh zhoulz@10.38.32.178 /home/zhoulz/ppat/startPPAT.sh $a
+  exit 0
+else
+  echo "odvb failure"
+  exit 1
+fi
