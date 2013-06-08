0b60a9eced8cee38abe23374a83f16ce0f5f3700
Yufei Shi <yfshi@marvell.com>
2013-06-08 13:25:54 +0800

    dailybuild: update new stdio log
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- android_full_build.sh
+++ android_full_build.sh
@@ -3,7 +3,8 @@ ids=$1
 echo Build Branch: $1
 ids_2=$2
 echo PLATFORM_ANDROID_VARIANT: $2
-rm -f /home/buildfarm/buildbot_script/args.log
+STD_LOG=/home/buildfarm/buildbot_script/stdio.log
+rm -f $STD_LOG
 . ~/buildbot_script/buildbot/core/check_update.sh
 cd ~/aabs
 if [ "$ids_2" != "" -a "$ids_2" != "None" ]; then
@@ -16,44 +17,41 @@ if [ ! "${var_0}" == "rls" ]; then
   product=`echo ${ids#*_}`
   last_build="/autobuild/android/${platform}/LAST_BUILD.${platform}_${product}"
   target=${platform}-${product}
-  echo "platform $platform" | tee -a /home/buildfarm/buildbot_script/args.log
-  echo "product $product" | tee -a /home/buildfarm/buildbot_script/args.log
-  echo "last_build $last_build" | tee -a /home/buildfarm/buildbot_script/args.log
+  echo "platform $platform" | tee -a $STD_LOG
+  echo "product $product" | tee -a $STD_LOG
+  echo "last_build $last_build" | tee -a $STD_LOG
   echo $target
 else
   var_1=`echo ${ids#*_}`
   platform=`echo ${var_1%%_*}`
-  echo "platform $platform" | tee -a /home/buildfarm/buildbot_script/args.log
+  echo "platform $platform" | tee -a $STD_LOG
   last=`echo ${var_1#*_}`
   product=`echo ${last%%_*}`
-  echo "product $product" | tee -a /home/buildfarm/buildbot_script/args.log
+  echo "product $product" | tee -a $STD_LOG
   last=`echo ${last#*_}`
   if [ "$product" = "${last}" ]; then
     target="$platform-$product"
     last_build="/autobuild/android/${platform}/LAST_BUILD.${platform}-${product}"
   else
-    echo "last $last" | tee -a /home/buildfarm/buildbot_script/args.log
+    echo "last $last" | tee -a $STD_LOG
     target="$platform-$product:$last"
     last_build="/autobuild/android/${platform}/LAST_BUILD.rls_${platform}_${product}_${last}"
   fi
-  echo "last_build $last_build" | tee -a /home/buildfarm/buildbot_script/args.log
+  echo "last_build $last_build" | tee -a $STD_LOG
   echo $target
 fi
-tools/build_platforms.sh ${target} | tee -a log.txt
-result=`grep ">PASS<" log.txt`
+tools/build_platforms.sh ${target} | tee -a $STD_LOG
+result=`grep ">PASS<" $STD_LOG`
 if [ -n "$result" ]; then
-  nobuild=`grep ">No build<" log.txt`
+  nobuild=`grep ">No build<" $STD_LOG`
   if [ -n "$nobuild" ]; then
-    rm -f log.txt
     echo "no build"
     exit 255
   else
     echo "success"
-    rm -f log.txt
     exit 0
   fi
 else
-  rm -f log.txt
   exit 1
   echo "failure"
 fi
