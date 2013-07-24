4e0097326cb137189ed5ac6e829b458c2f42847a
Yufei Shi <yfshi@marvell.com>
2013-07-24 13:18:25 +0800

    core: add new function for create csv for gerrit database
        create_csv [-o] <output file>
            [-b] {branch file from getname}
            [-p] {path file from getname}
            [--review=] R+-2/V+-2/S Sample --review=R+2/--review=S/--review=V-1
            R=Code Review V=Verified S=Submit
            [-h] help
    
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- core/create_csv.py
+++ core/create_csv.py
@@ -17,53 +17,94 @@ m_user = "buildfarm"
 #Code remote server
 m_remote_server = "shgit.marvell.com"
 
+#return table from gerrit database by singel patch path, branch, and patch status.
 def return_gerrit_changes(path, branch, gerrit_open):
     global m_user
     global m_remote_server
     args = "ssh -p 29418 " + m_user + "@" + m_remote_server + " gerrit gsql -c \"select\ *\ from\ changes\ WHERE\ dest_branch_name=\\\'refs/heads/" + branch +  "\\\'\ AND\ open=\\\'" + gerrit_open + "\\\'\ AND\ dest_project_name\ REGEXP\ \\\'" + path + "\\\'\""
-    return os.popen(args).read()
+    a = []
+    if path == "X" and branch == "X" and gerrit_open == "X":
+        f = open('tmptxt', 'w')
+        f.write(os.popen(args).read())
+        f.close()
+        in_txt = csv.reader(open("tmptxt", "rb"), delimiter = '|')
+        for row in in_txt:
+            a.append(row)
+        a.pop(1)
+        a.pop(len(a)-1)
+    else:
+        f = open('tmptxt', 'w')
+        f.write(os.popen(args).read())
+        f.close()
+        in_txt = csv.reader(open("tmptxt", "rb"), delimiter = '|')
+        for row in in_txt:
+            a.append(row)
+        a.pop(0)
+        a.pop(0)
+        a.pop(len(a)-1)
+    return a
 
+#setup gerrit patch change csv
 def setup_gerrit_changes_csv(fout, d_path, d_branch):
     out_csv = csv.writer(open(fout, 'wb'))
-    f = open('tmptxt', 'w')
-    f.write(return_gerrit_changes("X", "X", "X"))
-    f.close()
-    in_txt = csv.reader(open("tmptxt", "rb"), delimiter = '|')
-    a = []
-    for row in in_txt:
-        a.append(row)
-    a.pop(1)
-    a.pop(len(a)-1)
-    out_csv.writerows(a)
+    out_csv.writerows(return_gerrit_changes("X", "X", "X"))
     for name, c_path in d_path.items():
         branch = d_branch.get(name)
         print name
         print branch
-        f = open('tmptxt', 'w')
-        f.write(return_gerrit_changes(c_path, branch, "Y"))
-        in_txt = csv.reader(open("tmptxt", "rb"), delimiter = '|')
-        f.close()
+        out_csv.writerows(return_gerrit_changes(c_path, branch, "Y"))
+
+#return array via matching patch_set_approvals
+def return_via_patch_approvals(in_array, category, value, change_open):
+    global m_user
+    global m_remote_server
+    out_array = []
+    out_array.extend(in_array)
+    for rows in in_array:
         a = []
+        args = "ssh -p 29418 " + m_user + "@" + m_remote_server + " gerrit gsql -c \"select\ *\ from\ patch_set_approvals\ WHERE\ VALUE=\\\'" + value +  "\\\'\ AND\ CHANGE_OPEN=\\\'" + change_open + "\\\'\ AND\ CATEGORY_ID=\\\'" + category + "\\\'\ AND\ CHANGE_ID=\\\'" + rows[14].strip() + "\\\'\""
+        f = open('tmptxt', 'w')
+        f.write(os.popen(args).read())
+        f.close()
+        in_txt = csv.reader(open("tmptxt", "rb"), delimiter = '|')
         for row in in_txt:
             a.append(row)
-        a.pop(0)  
+        a.pop(0)
         a.pop(0)
         a.pop(len(a)-1)
-        out_csv.writerows(a)
+        if not a:
+            print "changeid with [" + rows[14].strip() + "] is empty patch_set_approvals"
+            out_array.remove(rows)
+    return out_array
+
+#setup gerrit patch change csv with patch_set_approvals
+def setup_gerrit_changes_via_approvals(fout, d_path, d_branch, category, value):
+    out_csv = csv.writer(open(fout, 'wb'))
+    out_csv.writerows(return_gerrit_changes("X", "X", "X"))
+    for name, c_path in d_path.items():
+        branch = d_branch.get(name)
+        print name
+        print branch
+        array_all = return_gerrit_changes(c_path, branch, "Y")
+        out_csv.writerows(return_via_patch_approvals(array_all, category, value, "Y"))
 
 #User help
 def usage():
     print "\tcreate_csv [-o] <output file>"
     print "\t      [-b] {branch file from getname}"
     print "\t      [-p] {path file from getname}"
+    print "\t      [--review=] R+-2/V+-2/S Sample --review=R+2/--review=S/--review=V-1"
+    print "\t              R=Code Review V=Verified S=Submit"
     print "\t      [-h] help"
 
 def main(argv):
     fout = ""
     branch_list = ""
     path_list = ""
+    review_value = ""
+    review_category = ""
     try:
-        opts, args = getopt.getopt(argv, "o:b:p:h")
+        opts, args = getopt.getopt(argv, "o:b:p:h",["review="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -80,6 +121,34 @@ def main(argv):
         elif opt in ("-p"):
             path_list = arg
             print path_list
+        elif opt in ("--review"):
+            print arg
+            if arg[1:2] == "" and arg[:1] == "S":
+                review_value = "1"
+                review_category = "SUBM"
+            elif arg[1:2] == "+" and (arg[2:] == "1" or arg[2:] == "2"):
+                review_value = arg[2:]
+                if arg[:1] == "R":
+                    review_category = "CRVW"
+                elif arg[:1] == "V":
+                    review_category = "VRIP"
+                else:
+                    usage()
+                    sys.exit(2)
+            elif arg[1:2] == "-" and (arg[2:] == "1" or arg[2:] == "2"):
+                review_value = arg[1:]
+                if arg[:1] == "R":
+                    review_category = "CRVW"
+                elif arg[:1] == "V":
+                    review_category = "VRIP"
+                else:
+                    usage()
+                    sys.exit(2)
+            else:
+                usage()
+                sys.exit(2)
+            print review_category
+            print review_value
     if fout == "" or branch_list == "" or path_list == "":
         usage()
         sys.exit(2)
@@ -89,7 +158,10 @@ def main(argv):
     fp = open(branch_list, "r")
     d_branch = pickle.load(fp)
     fp.close()
-    setup_gerrit_changes_csv(fout, d_path, d_branch)
+    if not review_category:
+        setup_gerrit_changes_csv(fout, d_path, d_branch)
+    else:
+        setup_gerrit_changes_via_approvals(fout, d_path, d_branch, review_category, review_value)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
