facb7afcb7c55d7a3fde671c40d29fb99e8750c1
Yufei Shi <yfshi@marvell.com>
2013-11-04 17:49:50 +0800

    core: update rtvb_patches for match more case
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- core/rtvb_patches.sh
+++ core/rtvb_patches.sh
@@ -6,15 +6,15 @@ removte_server=shgit
 
 rev_current=$(git log -1 --pretty=format:%H)
 branch_manifest=$REPO_RREV
-rev_manifest=$(git log -1 --pretty=format:%H $removte_server/$branch_manifest)
+
+if [[ $branch_manifest =~ ^[0-9a-z]+$ && ${#branch_manifest} -eq 40 ]]; then
+    rev_manifest=$branch_manifest
+else
+    rev_manifest=$(git log -1 --pretty=format:%H $removte_server/$branch_manifest)
+fi
 #rev_manifest=$(repo info ./ | grep "Current revision" | awk -F"Current revision: " '{ print $2 }')
 
-#echo -----------------------------------------------
-
 if [ "$rev_current" != "$rev_manifest" ]; then
-    #echo "${REPO_PATH}: no change"
-#else
-    #echo "${REPO_PATH}:"
     git log --pretty=format:%H ${rev_manifest}..${rev_current}
     echo " "
 fi
