a2ef6820f18fe432c8488945c4634f54ae4ced81
Yufei Shi <yfshi@marvell.com>
2013-11-06 15:40:28 +0800

    main: RTVB add rtvb enterance
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

---
+++ rt_virtual_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -o pipefail
+STD_LOG=/home/buildfarm/buildbot_script/stdio.log
+rm $STD_LOG
+. ~/buildbot_script/buildbot/core/check_update.sh | tee -a $STD_LOG
+cd ~/buildbot_script/buildbot
+. ~/buildbot_script/buildbot/dev/real_time_virtual_build.sh $@ | tee -a $STD_LOG
