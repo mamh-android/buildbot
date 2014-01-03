57ac788d7af562f553543812849309b871e527bc
buildfarm <buildfarm@marvell.com>
2014-01-03 11:12:50 +0800

    core: version 1.1 gerrit_pick_patch_function
        1. support topic for RTVB
        2. decrease the impact of gerrit load (less impact for gerrit web)
    
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- core/gerrit_pick_patch_new.py
+++ core/gerrit_pick_patch_new.py
@@ -56,8 +56,6 @@ def return_gerrit_query_jsonstr(revisions):
 
 #Generate and return the args[i] from list jsonstr for git cherry pick
 def args_from_jsonstr_list(jsonstr_list):
-#    if (gerrit_patch_object == ""): sys.exit()
-#git fetch ssh://buildfarm@shgit.marvell.com:29418/android/platform/frameworks/opt/telephony refs/changes/71/82171/1 && git cherry-pick FETCH_HEAD
     args = [[]] * len(jsonstr_list)
     for i in range(len(jsonstr_list)):
         projectspec = jsonstr_list[i]['project']
@@ -75,20 +73,24 @@ def args_from_jsonstr_list(jsonstr_list):
     return args
 
 #Generate and return the args[i] from gerrit_patch_object for git check out for rtvb
-def args_from_gerrit_patch_object_rtvb(jsonstr_list):
+def args_from_jsonstr_list_rtvb(jsonstr_list):
     args = [[]] * len(jsonstr_list)
     for i in range(len(jsonstr_list)):
         projectspec = jsonstr_list[i]['project']
         r_dest_project_name = projectspec
         refspec = jsonstr_list[i]['patchSets'][0]['ref']
+        if jsonstr_list[i].has_key('topic'):
+            checkspec = "cherry-pick"
+        else:
+            checkspec = "checkout"
         if r_dest_project_name[:25] == "android/platform/manifest":
             cd_dest_project_name = ".repo/manifests"
             cd_args = "cd " + cd_dest_project_name + ";"
-            gitcp_args = "git fetch ssh://" + m_user + "@" + m_remote_server + ":29418/" + r_dest_project_name + " " + refspec + " && git checkout FETCH_HEAD;cd -;ln -sf manifests/default.xml .repo/manifest.xml;repo sync;"
+            gitcp_args = "git fetch ssh://" + m_user + "@" + m_remote_server + ":29418/" + r_dest_project_name + " " + refspec + " && git " + checkspec + " FETCH_HEAD;cd -;ln -sf manifests/default.xml .repo/manifest.xml;repo sync;"
         else:
             cd_dest_project_name = generate_path_rtvb(r_dest_project_name)
             cd_args = "cd " + cd_dest_project_name + ";"
-            gitcp_args = "git fetch ssh://" + m_user + "@" + m_remote_server + ":29418/" + r_dest_project_name + " " + refspec + " && git checkout FETCH_HEAD;"
+            gitcp_args = "git fetch ssh://" + m_user + "@" + m_remote_server + ":29418/" + r_dest_project_name + " " + refspec + " && git " + checkspec + " FETCH_HEAD;"
         args[i] = cd_args + gitcp_args
     return args
 
@@ -96,8 +98,7 @@ def args_from_gerrit_patch_object_rtvb(jsonstr_list):
 def generate_path(r_dest_project_name):
     manifest_xml_path = ""
     manifest_xml_name = ""
-    #manifest_file = "/home/buildfarm/aabs/odvb_work/manifest.xml"
-    manifest_file = "/home/yfshi/workspace/eden-jb43/.repo/manifest.xml"
+    manifest_file = "/home/buildfarm/aabs/odvb_work/manifest.xml"
     search = ""
     pat = '\spath=\"([a-zA-Z0-9-_/]*)\"\s'
     if r_dest_project_name[:25] == "android/platform/manifest":
@@ -137,7 +138,6 @@ def generate_path(r_dest_project_name):
 def generate_path_rtvb(r_dest_project_name):
     manifest_xml_path = ""
     manifest_xml_name = ""
-    #manifest_file = "/home/buildfarm/aabs/odvb_work/manifest.xml"
     manifest_file = "/home/buildfarm/aabs/rtvb_work/.repo/manifest.xml"
     search = ""
     pat = '\spath=\"([a-zA-Z0-9-_/]*)\"\s'
@@ -182,13 +182,35 @@ def return_gerrit_revisions(gerrit_patch_file):
         json_list.append(json.loads(jsonstr))
     infile.close()
 #read first revision and remove it from list
-    revisions = json_list[0]['currentPatchSet']['revision']
-    json_list.pop(0)
+    revisions = []
+    if not json_list[0].has_key('topic'):
+        revisions.append(json_list[0]['currentPatchSet']['revision'])
+    else:
+        print "cherry pick patches with topic:%s" % (json_list[0]['topic'])
+        revisions = return_revisions_from_topic(json_list[0]['topic'])
+#remove the first line and dump it to file
     outfile = open(gerrit_patch_file, 'w')
+    json_list.pop(0)
     for json_str in json_list:
         json.dump(json_str, outfile)
         outfile.write('\n')
     outfile.close()
+    return revisions
+
+#Return revision list from topic
+def return_revisions_from_topic(topic):
+    revisions = []
+    cmd = "ssh -p 29418 %s@%s gerrit query --current-patch-set --format=JSON topic:%s" % (m_user, m_remote_server, topic)
+    (status, remote_output) = run_command_status(cmd)
+    json_list = []
+    for output in remote_output.split('\n'):
+        jsonstr = json.loads(output)
+        if not jsonstr.has_key('runTimeMilliseconds'):
+            json_list.append(jsonstr)
+#sorted by createdOn
+    json_list = sorted(json_list, key=lambda patch: patch['currentPatchSet']['createdOn'])
+    for jsonstr in json_list:
+        revisions.append(jsonstr['currentPatchSet']['revision'])
     return revisions
 
 #Run args[i] by shell
@@ -201,7 +223,7 @@ def run_args(args):
 def usage():
     print "\tgerrit_pick_patch"
     print "\t      [-p] <gerrit patchset list except patches for manifest> Eks: 001,002,003 the list is splited by ,"
-    print "\t      [-t] <gerrit patch json tab file> "
+    print "\t      [-t] <gerrit patch json file> "
     print "\t          the function design for rtvb"
     print "\t          Check-out the patch instead cherry-pick, pick the first patch from tab file, and remove it from tab file"
     print "\t      [--showonly] only show the patches from listed patchsetID"
@@ -209,7 +231,7 @@ def usage():
 
 def main(argv):
     gerrit_patch_list = ""
-    gerrit_patch_tab = ""
+    gerrit_patch_file = ""
     args_list = []
     try:
         opts, args = getopt.getopt(argv, "p:t:h")
@@ -223,17 +245,17 @@ def main(argv):
         elif opt in ("-p"):
             gerrit_patch_list = arg.split(',')
         elif opt in ("-t"):
-            gerrit_patch_tab = arg
+            gerrit_patch_file = arg
 
-    if (gerrit_patch_list == "") and (gerrit_patch_tab == ""):
+    if (gerrit_patch_list == "") and (gerrit_patch_file == ""):
         usage()
         sys.exit(2)
 
     if (gerrit_patch_list != ""):
         args_list = args_from_jsonstr_list(return_gerrit_query_jsonstr(gerrit_patch_list))
         print args_list
-    elif (gerrit_patch_tab != ""):
-        args_list = args_from_jsonstr_list_rtvb(return_gerrit_query_jsonstr(return_gerrit_revisions(gerrit_patch_tab)))
+    elif (gerrit_patch_file != ""):
+        args_list = args_from_jsonstr_list_rtvb(return_gerrit_query_jsonstr(return_gerrit_revisions(gerrit_patch_file)))
         print args_list
 
     run_args(args_list)
