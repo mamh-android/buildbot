b955cfb6c90f3427ff915018c7ac27c7bf62f893
Yufei Shi <yfshi@marvell.com>
2014-02-12 17:47:43 +0800

    core: added a script to cherry-pick the gerrit patch
        Script run diagram
        1. git fetch
        2. git checkout <gerrit-commit>
        3. generate a list for <gerrit-commit> dependencies and gerrit status open
        4. git reset --hard orignal/master
        5. git cherry-pick list
    
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

---
+++ core/cherry_pick_open_patch.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+# v1.0
+#    Author: yfshi@marvell.com
+
+import subprocess
+import getopt
+import pickle
+import re
+import sys
+import os
+import csv
+import json
+import shlex
+import datetime
+from datetime import datetime, timedelta
+
+#Gerrit admin user
+m_user = "buildfarm"
+
+#Code remote server
+m_remote_server = "privgit.marvell.com"
+
+cosmo_branch = "origin/master"
+
+VERBOSE = False
+
+def run_command_status(*argv, **env):
+    if VERBOSE:
+        print(datetime.datetime.now(), "Running:", " ".join(argv))
+    if len(argv) == 1:
+        argv = shlex.split(str(argv[0]))
+    newenv = os.environ
+    newenv.update(env)
+    p = subprocess.Popen(argv, stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT, env=newenv)
+    (out, nothing) = p.communicate()
+    out = out.decode('utf-8')
+    return (p.returncode, out.strip())
+
+def run_command(*argv, **env):
+    (rc, output) = run_command_status(*argv, **env)
+    return output
+
+#return a reversed open patch list
+def return_dependencies_list():
+    depend_list = []
+    i = 1
+    while True:
+        arg = "git log -%s " % (i) + "--pretty=format:%H"
+        (status, remote_output) = run_command_status(arg)
+        revision = remote_output.split('\n')[i-1]
+        if return_gerrit_patch_status(revision):
+            depend_list.append(revision)
+            i=i+1
+        else:
+            break
+    depend_list.reverse()
+    return depend_list
+
+#Return gerrit patch status. True for open, False for none-open
+def return_gerrit_patch_status(revision):
+    cmd = "ssh -p 29418 %s@%s gerrit query --current-patch-set --format=JSON commit:%s is:open" % (m_user, m_remote_server, revision)
+    (status, remote_output) = run_command_status(cmd)
+    jsonstr = json.loads(remote_output.split('\n')[0])
+    return not jsonstr.has_key('runTimeMilliseconds')
+
+#re-setup code by cherry-pick the patches
+def setup_code_by_cherry_pick(revisions):
+    arg = "git reset --hard %s" % (cosmo_branch)
+    subprocess.check_call(arg, shell=True)
+    for rev in revisions:
+        arg = "git cherry-pick %s" % (rev)
+        (status, remote_output) = run_command_status(arg)
+        if status:
+            print "cherry-pick failed when cherry pich %s" % (rev)
+            print remote_output
+            exit(1)
+    print "git cherry-pick completed"
+
+#checkout the commit
+def gerrit_checkout(revision):
+    cmd = "ssh -p 29418 %s@%s gerrit query --current-patch-set --format=JSON commit:%s" % (m_user, m_remote_server, revision)
+    (status, remote_output) = run_command_status(cmd)
+    jsonstr = json.loads(remote_output.split('\n')[0])
+    arg = "git fetch ssh://%s@%s:29418/cosmo %s && git checkout FETCH_HEAD" % (m_user, m_remote_server, jsonstr['currentPatchSet']['ref'])
+    print arg
+    subprocess.check_call(arg, shell=True)
+
+def run(revision):
+   arg = "git fetch origin"
+   subprocess.check_call(arg, shell=True)
+   arg = "git reset --hard %s" % (cosmo_branch)
+   subprocess.check_call(arg, shell=True)
+   gerrit_checkout(revision)
+   rev_list = return_dependencies_list()
+   setup_code_by_cherry_pick(rev_list)
+
+#User help
+def usage():
+    print "\tcherry-pick all the dependencies patch and gerrit status open patch [-c] commitID"
+    print "\t      [-h] help"
+
+def main(argv):
+    commit_id = ""
+    try:
+        opts, args = getopt.getopt(argv, "c:h")
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h"):
+            usage()
+            sys.exit()
+        elif opt in ("-c"):
+            commit_id = arg
+            print commit_id
+
+    if commit_id == "":
+        usage()
+        sys.exit(2)
+    run(commit_id)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
