89b86b72778c673cec6b5776f5034de290f59eef
Yufei Shi <yfshi@marvell.com>
2014-04-10 16:43:00 +0800

    cosmoimauto: Updated script to auto
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- Load_imauto_after_cosmo.py
+++ Load_imauto_after_cosmo.py
@@ -10,14 +10,19 @@ import getopt
 import datetime
 from datetime import date
 import glob
+import shutil
 import smtplib
 import ConfigParser
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-BUILDBOT_URL = "http://buildbot.marvell.com:8010/builders/image_auto_test/builds/"
+BUILDBOT_URL = "http://buildbot.marvell.com:8010/builders/cosmo_image_auto_test/builds/"
 IMAUTO_LOG = ".imauto.build.log"
 TEST_CFG = "test.txt"
+IMAGE_SERVER = "\\\\sh-srv06\\cosmo_build\\"
+OUTPUTIMAGES = "\\cosmobuild\\test\\OutputImages\\"
+OUTPUT_IMAUTO_SERVER = "\\\\sh-srv06\\cosmo_imauto\\"
+BUILD_TYPE = "cosmo-imauto"
 
 #Gerrit admin user
 ADM_USER = "buildfarm"
@@ -30,7 +35,6 @@ SMTP_SERVER = "10.68.76.51"
 
 #Buildfarm Maintainer
 BF_ADMIN = "yfshi@marvell.com"
-CAM_QAE_ADMIN = "yzhan45@marvell.com"
 
 #Mail list
 MAIL_LIST = []
@@ -50,14 +54,17 @@ sys.stdout = flushfile(sys.stdout)
 def return_mail_text(build_type, branch, build_nr, result, failurelog, cfg_file, image_link=None):
     subject = "[imauto-%s][%s] %s %s" % (branch, str(date.today()), build_type, result)
     message =  "This is an automated email from auto build system.\n"
-    message += "It was generated because %s %s\n\n" % (build_type, result)
+    message += "It was generated because %s was %s\n\n" % (build_type, result)
     message += "Buildbot Url:\n%s%s\n\n" % (BUILDBOT_URL, build_nr)
     if (result == 'failed'):
         message += "CFG file is followed:\n%s\n\n" % cfg_file
         message += "Last part of the build log is followed:\n%s\n\n" % failurelog
     if (result == 'success'):
         message += "CFG file is followed:\n%s\n\n" % cfg_file
-        message += "You can download the package at:\n\"%s\"\n\n" % (image_link)
+        message += "You get imauto report at:\n"
+        for r in image_link:
+            message += "%s\n" % r
+        message += "\n\n"
     message +="Regards,\nTeam of CameraQAE\n"
     return subject, message
 
@@ -88,7 +95,44 @@ def return_failure_log(logfile):
                 failure_log = failure_log + log
     return failure_log
 
-def setup_testfile(filename, path, sensor, resolution, focus, testimage=None, isp=None, FWVersion=None, CalVersion=None):
+# return *_imauto test list from the last successful build
+def return_last_image_imauto(branch):
+    f = open('%sLAST_BUILD.%s' % (IMAGE_SERVER, branch), 'r')
+    l = f.readlines()
+    f.close()
+    last_package = l[1].split()[1]
+    print last_package
+    imauto_l = glob.glob("%s%s*_imauto" % (last_package, OUTPUTIMAGES))
+    print imauto_l
+    return imauto_l
+
+#create a folder according to git branch name and time.today
+def check_publish_folder(IMAGE_SERVER, branch):
+    today = date.today()
+    folder = str(today) + "_" + branch
+    folder_server = IMAGE_SERVER + folder
+    f = folder_server
+    if not os.path.isdir(f):
+        #os.mkdir(f)
+        return f
+    else:
+        i=1
+        while os.path.isdir(folder_server):
+            f = folder_server + "_" + str(i)
+            if not os.path.isdir(f):
+                #os.mkdir(f)
+                return f
+                break
+            else:
+                i=i+1
+
+# copy OutputImages *_imauto to OUTPUT_IMAUTO_SERVER
+def copy_outputimage(src, out):
+    dst = "%s\\%s" % (out, src.split('\\')[len(src.split('\\'))-1])
+    shutil.copytree(src,dst)
+    return dst
+
+def setup_testfile(filename, path, sensor=None, resolution=None, focus=None, testimage=None, isp=None, FWVersion=None, CalVersion=None):
     config = ConfigParser.RawConfigParser()
     config.add_section('optional')
     config.set('optional', 'Testimage', testimage)
@@ -109,69 +153,50 @@ def setup_testfile(filename, path, sensor, resolution, focus, testimage=None, is
 
 def run(build_nr, cfg_file, image_link, run_type='1', branch='master'):
     # git reset --hard branch
-    print "[imauto][%s] Start git reset --hard %s" % (str(datetime.datetime.now()), branch)
+    print "[%s][%s] Start git reset --hard %s" % (BUILD_TYPE, str(datetime.datetime.now()), branch)
     c_gitfetch = ['git', 'fetch', 'origin']
     ret = os.system(' '.join(c_gitfetch))
     c_resetbranch = ['git', 'reset', '--hard', 'origin/%s' % branch]
     ret = os.system(' '.join(c_resetbranch))
     if not (ret==0):
-        print "[imauto][%s] Failed git reset --hard" % (str(datetime.datetime.now()))
+        print "[%s][%s] Failed git reset --hard" % (BUILD_TYPE, str(datetime.datetime.now()))
         subject, text = return_mail_text('git-reset', branch, build_nr, 'failed', None, None, None)
         send_html_mail(subject,ADM_USER,BF_ADMIN.split(),text)
         exit(1)
-    print "[imauto][%s] End git reset --hard" % (str(datetime.datetime.now()))
+    print "[%s][%s] End git reset --hard" % (BUILD_TYPE, str(datetime.datetime.now()))
     # imauto run
-    print "[imauto][%s] Start imauto" % (str(datetime.datetime.now()))
+    print "[%s][%s] Start imauto" % (BUILD_TYPE, str(datetime.datetime.now()))
     c_imauto = ['perl test.pl %s %s' % (cfg_file, run_type)]
     print c_imauto
     ret = os.system(' '.join(c_imauto))
     if not (ret==0):
-        print "[imauto][%s] Failed imauto" % (str(datetime.datetime.now()))
+        print "[%s][%s] Failed imauto" % (BUILD_TYPE, str(datetime.datetime.now()))
         failure_log = return_failure_log(IMAUTO_LOG)
         cfg_file = return_failure_log(TEST_CFG)
-        subject, text = return_mail_text('[imauto]', branch, build_nr, 'failed', failure_log, cfg_file,None)
+        subject, text = return_mail_text('image-auto-test', branch, build_nr, 'failed', failure_log, cfg_file,None)
         send_html_mail(subject,ADM_USER,MAIL_LIST,text)
         exit(1)
     # All success
-    print "[imauto][%s] End Autotest" % (str(datetime.datetime.now()))
-    print "[imauto][%s] All success" % (str(datetime.datetime.now()))
+    print "[%s][%s] End Autotest" % (BUILD_TYPE, str(datetime.datetime.now()))
+    print "[%s][%s] All success" % (BUILD_TYPE, str(datetime.datetime.now()))
     cfg_file = return_failure_log(TEST_CFG)
-    subject, text = return_mail_text('[imauto]', branch, build_nr, 'success', None, cfg_file, image_link)
+    subject, text = return_mail_text('image-auto-test', branch, build_nr, 'success', None, cfg_file, image_link)
     send_html_mail(subject,ADM_USER,MAIL_LIST,text)
     exit(0)
 
 #User help
 def usage():
-    print "\timauto.py"
-    print "\t      [mandatory] section"
-    print "\t      [-p] Path sample: \\\\10.38.34.209\\home\\home\\wangnian\\git_folder\\multimedia_testsuite\\tools\\IMAUTO\\OriImg"
-    print "\t      [-s] Sensor {3H5#8}"
-    print "\t      [-r] Resolution {8M|2M|5M|8M|13M}"
-    print "\t      [-f] Focus {AF|FF}"
-    print "\t      [-n] build_nr"
-    print "\t      [optional] section"
-    print "\t      [-b] imauto branch defult=master"
-    print "\t      [-t] TestImage sample: 2014-02-18_pxa988-kk4.4"
-    print "\t      [-i] ISP {Dx0}"
-    print "\t      [-w] FWVersion {0215delivery}"
-    print "\t      [-c] CalVersion {0215delivery}"
+    print "\tdepend cosmo build imauto"
+    print "\t      [-b] branch"
     print "\t      [-m] email for notification"
     print "\t      [-h] help"
 
 def main(argv):
-    path = ""
-    sensor = ""
-    resolution = ""
-    focus = ""
-    build_nr = ""
     branch = ""
-    testimage = ""
-    isp = ""
-    fwversion = ""
-    calversion = ""
     email = ""
+    build_nr = ""
     try:
-        opts, args = getopt.getopt(argv,"p:s:r:f:n:b:t:i:w:c:m:h")
+        opts, args = getopt.getopt(argv,"b:n:m:h")
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -179,36 +204,25 @@ def main(argv):
         if opt in ("-h"):
             usage()
             sys.exit()
-        elif opt in ("-p"):
-            path = arg
-        elif opt in ("-s"):
-            sensor = arg
-        elif opt in ("-r"):
-            resolution = arg
-        elif opt in ("-f"):
-            focus = arg
-        elif opt in ("-n"):
-            build_nr = arg
         elif opt in ("-b"):
             branch = arg
-        elif opt in ("-t"):
-            testimage = arg
-        elif opt in ("-i"):
-            isp = arg
-        elif opt in ("-w"):
-            fwversion = arg
-        elif opt in ("-c"):
-            calversion = arg
+        elif opt in ("-n"):
+            build_nr = arg
         elif opt in ("-m"):
             email = arg
             MAIL_LIST.append(email)
 
-    if not path or not sensor or not resolution or not focus:
+    if not branch:
         usage()
         sys.exit(2)
 
-    setup_testfile(TEST_CFG, path, sensor, resolution, focus, testimage, isp, fwversion, calversion)
-    run(build_nr, TEST_CFG, path, '2')
+    var_path = check_publish_folder(OUTPUT_IMAUTO_SERVER, branch)
+    print var_path
+    outputimage_l = return_last_image_imauto(branch)
+    for i in outputimage_l:
+        path = copy_outputimage(i, var_path)
+        setup_testfile(TEST_CFG, path)
+        run(build_nr, TEST_CFG, path, '2', branch)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
