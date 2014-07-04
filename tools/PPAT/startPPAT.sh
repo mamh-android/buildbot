7caf646852a358c3f6b7673c7845ea157921dde1
Yufei Shi <yfshi@marvell.com>
2014-07-04 15:55:42 +0800

    tools: ppat updated start_ppat script for new jenkins trigger.py
    Change-Id: I62ccc980ebe67108e1446a67f3963727588a847d
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- tools/PPAT/startPPAT.sh
+++ tools/PPAT/startPPAT.sh
@@ -1,42 +1,25 @@
 #!/bin/bash
 set -o pipefail
+TRIGGER=/home/buildfarm/buildbot/slave/ppat_test/build_script/trigger.py
 STD_LOG=/home/buildfarm/buildbot_script/stdio.log
 PACKAGE_LINK=$( awk -F"<result-dir>http:|</result-dir>" ' /<result-dir>/ { print $2 } ' $STD_LOG )
-IDIR="PROPERTY_DIR"
-USER="buildfarm"
-PPAT_PXA988_SERVER="10.38.32.98"
-PPAT_EDEN_SERVER="10.38.32.98"
+#IDIR="PROPERTY_DIR"
+#USER="buildfarm"
+#PPAT_PXA988_SERVER="10.38.32.98"
+#PPAT_EDEN_SERVER="10.38.32.98"
 
-ids_0=$1
-ids_1=$2
-ids_2=$3
-ids_3=$PACKAGE_LINK
-ids_4=$5
-ids_5=$6
-ids_6=$7
-ids_7=$8
-ids_8=$9
-ids_9=${10}
-ids_10=${11}
-ids_11=${12}
-ids_12=${13}
-ids_13=${14}
-ids_14=${15}
-ids_15="'"${16}"'"
-ids_16=${17}
-ids_17="'"${18}"'"
+ids_1="'"$1"'"
+ids_2="'"$2"'"
+ids_3="'"$3"'"
+ids_4="'"$4"'"
+ids_5="'"$5"'"
 
 result=`grep ">PASS<" $STD_LOG`
 if [ -n "$result" ]; then
   echo "************start PPAT with parameters*************"
-  echo $ids_0 $ids_1 $ids_2 $ids_3 $ids_4 $ids_5 $ids_6 $ids_7 $ids_8 $ids_9 $ids_10 $ids_11 $ids_12 $ids_13 $ids_14 $ids_15 $ids_16 $ids_17
-  if [ $ids_5 = "pxa988-jb4.3" ]; then
-    ssh $USER@$PPAT_PXA988_SERVER /home/buildfarm/ppat/startPPAT.sh $ids_0 $ids_1 $ids_2 $ids_3 $ids_4 $ids_5 $ids_6 $ids_7 $ids_8 $ids_9 $ids_10 $ids_11 $ids_12 $ids_13 $ids_14 $ids_15 $ids_16 $ids_17
-    exit 0
-  elif [ $ids_5 = "eden-jb43" ]; then
-    ssh $USER@$PPAT_EDEN_SERVER /home/buildfarm/ppat/startPPAT.sh $ids_0 $ids_1 $ids_2 $ids_3 $ids_4 $ids_5 $ids_6 $ids_7 $ids_8 $ids_9 $ids_10 $ids_11 $ids_12 $ids_13 $ids_14 $ids_15 $ids_16 $ids_17
-    exit 0
-  fi
+  echo $PACKAGE_LINK $ids_1 $ids_2 $ids_3 $ids_4 $ids_5
+  $TRIGGER --imagepath $PACKAGE_LINK --device $ids_1 --blf $ids_2 --assigner $ids_3 --testcase $ids_4 --purpose $ids_5 --mode manual
+  exit 0
 else
   echo "odvb failure"
   exit 1
