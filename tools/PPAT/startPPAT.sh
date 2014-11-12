7b6198175e3d154c177483aebfa9650a83993b7a
Yufei Shi <yfshi@marvell.com>
2014-11-12 15:15:32 +0800

    tools: startPPAT ODVB script updated
    Change-Id: Ieefd45f9fe0c37ae4fae73c3f2a25188bde58007
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- tools/PPAT/startPPAT.sh
+++ tools/PPAT/startPPAT.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o pipefail
-TRIGGER=/home/buildfarm/buildbot/slave/ppat_test/build_script/trigger.py
+TRIGGER=/home/buildfarm/buildbot/slave/on_demand_virtual_build_with_ppat/build_script/trigger.py
 STD_LOG=/home/buildfarm/buildbot_script/stdio.log
 PACKAGE_LINK=$( awk -F"<result-dir>http:|</result-dir>" ' /<result-dir>/ { print $2 } ' $STD_LOG )
 #IDIR="PROPERTY_DIR"
@@ -8,17 +8,17 @@ PACKAGE_LINK=$( awk -F"<result-dir>http:|</result-dir>" ' /<result-dir>/ { print
 #PPAT_PXA988_SERVER="10.38.32.98"
 #PPAT_EDEN_SERVER="10.38.32.98"
 
-ids_1="'"$1"'"
-ids_2="'"$2"'"
-ids_3="'"$3"'"
-ids_4="'"$4"'"
-ids_5="'"$5"'"
+ids_1=$1
+ids_2=$2
+ids_3=$3
+ids_4=$4
+ids_5=$5
 
 result=`grep ">PASS<" $STD_LOG`
 if [ -n "$result" ]; then
   echo "************start PPAT with parameters*************"
   echo $PACKAGE_LINK $ids_1 $ids_2 $ids_3 $ids_4 $ids_5
-  $TRIGGER --imagepath $PACKAGE_LINK --device $ids_1 --blf $ids_2 --assigner $ids_3 --testcase $ids_4 --purpose $ids_5 --mode manual
+  $TRIGGER --imagepath $PACKAGE_LINK --device $ids_1 --blf $ids_2 --assigner $ids_3 --testcase "$ids_4" --purpose "$ids_5" --mode manual
   exit 0
 else
   echo "odvb failure"
