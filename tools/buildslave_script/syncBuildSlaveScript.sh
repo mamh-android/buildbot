a0af2a9185f211d474b16e1893fb4fc8a3cf0fbb
Yufei Shi <yfshi@marvell.com>
2014-12-03 10:20:06 +0800

    tools: init syncBuildSlaveScript
    Change-Id: I84cda396f99aa722a176bad7614486f20d706d2e
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

---
+++ tools/buildslave_script/syncBuildSlaveScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# V1: 2014-12-3:Yufei: initial code
+
+set -e
+
+VERSION=1
+BUILDSLAVE_LIST="bf-c1 bf-c2 bf-c3 bf-c4 bf-c5 bf-f1 bf-f2 bf-f3"
+SCRIPT_PATH=/home/buildfarm/buildbot_script/buildbot
+GIT_SYNC="cd $SCRIPT_PATH && git fetch origin;cd $SCRIPT_PATH && git checkout origin/master;"
+
+for i in $BUILDSLAVE_LIST; do
+    echo ====================================
+    echo sync BuildScript of buildslave $i
+    ssh buildfarm@$i "$GIT_SYNC"
+done
