de4103ff736a43faddca37686963c26ddaee0b82
Yufei Shi <yfshi@marvell.com>
2014-12-25 14:17:36 +0800

    tools: merge tool v1.1, push the successful merge patch to remote
    Change-Id: I3be761bd543965c47bb55b3b4442e9243d59d3d4
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- tools/Merge_patch/merge.py
+++ tools/Merge_patch/merge.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # v1.0 Base function is ready
+# v1.1 Push to remote function enabled
 #    merge patch_list.diff
 
 import os
@@ -12,6 +13,9 @@ import csv
 SCRIPT_TYPE = 'Merging_Patch'
 C_PATH = os.getcwd()
 SCRIPT_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
+DREMOTE = None
+DBRANCH = None
+RENAME = False
 
 #Define patchInfo object array class
 class patchinfo:
@@ -56,6 +60,24 @@ class XXTree:
                 list.append(file)
         return list
 
+#git push
+def gitPush(project, remote, branch):
+    for i in range(5):
+        cmd = "cd %s;" % project
+        cmd += "git push %s HEAD:refs/heads/%s;" % (remote, branch)
+        status = os.system(cmd)
+        if (status==0):
+            print "Success: %s" %cmd
+            break
+        else:
+            print "Retry: %s" %cmd
+
+def clrEnv(project):
+    cmd = "cd %s;" % project
+    cmd += "git clean -f;"
+    cmd += "git reset --hard HEAD;"
+    status = os.system(cmd)
+
 #write to csv
 def patchToCsv(patchList, outputfile):
     out_csv = csv.writer(open(outputfile, 'wb'))
@@ -81,8 +103,15 @@ def run(input_file, output_file):
         status = os.system(cmd)
         if (status==0):
             i.status = 'P'
+            m = i.patch + '.merged'
+            if RENAME == True:
+                os.rename(i.patch, m)
+            if DREMOTE and DBRANCH:
+                print "publishing out"
+                gitPush(i.project, DREMOTE, DBRANCH)
         else:
             i.status = 'F'
+            clrEnv(i.project)
     #write to csv
     patchToCsv(patchList, output_file)
     print "[%s][%s] Completed" % (SCRIPT_TYPE, str(datetime.datetime.now()))
@@ -92,15 +121,24 @@ def run(input_file, output_file):
 #User help
 def usage():
     print "\tmerge.py"
+    print "\t       ===[mandatory section]==="
     print "\t      [-i] input patch.diff"
-    print "\t      [-o] output results file <out.csv>"
+    print "\t       ===[optional section]==="
+    print "\t      [-o] output results file by default <out.csv>"
+    print "\t       Only both remote and branch is available, the patch will be push out"
+    print "\t      [--remote={remote}]"
+    print "\t      [--branch={branch}]"
+    print "\t      [--rename] rename the patch if it's on"
     print "\t      [-h] help"
 
 def main(argv):
     input_file = None
     output_file = 'out.csv'
+    global DREMOTE
+    global DBRANCH
+    global RENAME
     try:
-        opts, args = getopt.getopt(argv,"i:o:h")
+        opts, args = getopt.getopt(argv,"i:o:h",["rename","remote=", "branch="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -112,6 +150,13 @@ def main(argv):
             input_file = arg
         elif opt in ("-o"):
             output_file = arg
+        elif opt in ("--remote"):
+            DREMOTE = arg
+        elif opt in ("--branch"):
+            DBRANCH = arg
+        elif opt in ("--rename"):
+            RENAME = True
+
     if not input_file:
         usage()
         sys.exit(2)
