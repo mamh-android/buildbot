71f617b1f242eaab6712d1d035194c629a1b3e73
Yufei Shi <yfshi@marvell.com>
2015-01-15 16:55:06 +0800

    Cosmo_daily: modfied MAIL_LIST
    Change-Id: Ib8a08a495622546fff2d53efc4b70f9004b8dae4
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- Cosmo_daily.py
+++ Cosmo_daily.py
@@ -40,7 +40,7 @@ BF_ADMIN = "yfshi@marvell.com"
 #MAIL_LIST = get_mail_list("cosmo-dev")
 MAIL_LIST = ['gr-apse-cosmo@marvell.com']
 #extend extra people who not in the gr-apse-cosmo
-MAIL_LIST.extend(['yfshi@marvell.com', 'skershaw@marvell.com', 'seankuo@marvell.com', 'pstsai@marvell.com', 'dongli@marvell.com', 'ejin@marvell.com'])
+MAIL_LIST.extend(['yfshi@marvell.com', 'skershaw@marvell.com', 'seankuo@marvell.com', 'pstsai@marvell.com', 'dongli@marvell.com', 'ejin@marvell.com', 'samson@marvell.com','chwu@marvell.com','hhornung@marvell.com'])
 
 ''' Force Python's print function to output to the screen.
 '''
