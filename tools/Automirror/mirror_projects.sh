56e129f88cca476e7a66a3531194676dad296c02
Yufei Shi <yfshi@marvell.com>
2015-02-17 14:22:21 +0800

    tools: Automirror add data print in the log
    Change-Id: Ic1c7d16d163000f0be307d4d92649f3ef087af10
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- tools/Automirror/mirror_projects.sh
+++ tools/Automirror/mirror_projects.sh
@@ -6,6 +6,12 @@ set -e
 PWD=$(pwd)
 LOGFILE=$PWD/"mirror-projects.log"
 
+echo ============= | tee -a $LOGFILE
+echo "[$(date)] start sync the projects" | tee -a $LOGFILE
+
 #mirror added 2015-02-17 owner Tingting wang (BSP)
 #git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
     cd /git/git/ose/linux/mirror/git.kernel.org/kernel/linux-stable.git && git fetch origin | tee -a $LOGFILE
+
+echo "[$(date)] completed sync the projects" | tee -a $LOGFILE
+echo ============= | tee -a $LOGFILE
