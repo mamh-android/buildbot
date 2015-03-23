a4d272070108bb5f196393bd7ad67f61028f1277
Yufei Shi <yfshi@marvell.com>
2015-03-23 10:30:26 +0800

    buildslave_script gerrit_lspermission updated
    Change-Id: I8c76f2246ad659c5e3fdbf79a3706ded9361c9c0
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

--- tools/buildslave_script/gerrit_lspermission.sh
+++ tools/buildslave_script/gerrit_lspermission.sh
@@ -7,3 +7,20 @@ for i in $list;do
 echo ====== $i ====== >> /tmp/me;
 ssh -p 29418 shgit.marvell.com gerrit ls-members $i | tee -a /tmp/me;
 done
+
+gerrit_permission(){
+for i in $(cat l); do
+    git fetch ssh://shgit.marvell.com/$i refs/meta/config && git reset --hard FETCH_HEAD;
+    if grep -q Permission_parent/All-android *;then
+      echo ===
+      echo yes
+      echo ===
+    else
+      echo ===
+      echo no
+      echo $i
+      cat config
+      echo ===
+    fi
+done
+}
