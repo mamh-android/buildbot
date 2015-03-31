7348b226811aaa7c1320e7d77592c89cce17968f
Yufei Shi <yfshi@marvell.com>
2015-03-31 11:35:46 +0800

    tools: buildslave script shgit_dump_db.sh backup
    Change-Id: I31be8d850550c875b75b4239defade4fc109853a
    Signed-off-by: Yufei Shi <yfshi@marvell.com>

---
+++ tools/buildslave_script/shgit_dump_db.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# V1: 2014-12-3:Yufei: initial code
+
+set -e
+
+VERSION=1
+DB_GIT_PATH="/home/gerrit2/backup_gerritdatabase/shgit_database/"
+PG_DUMP="pg_dump reviewdb_2015_01 > reviewdb.sql"
+
+#dump DB
+cd $DB_GIT_PATH && pg_dump reviewdb_2015_01 > reviewdb.sql
+RET=$?
+if [ $RET -ne 0 ]; then
+    echo "failed on dumping the databases"
+    echo "exit value:" $RET
+    exit 1
+fi
+            
+#push to git
+git commit -a -s -m "Autodump: $PG_DUMP `date --rfc-3339=seconds`" && git push origin HEAD:master
+RET=$?
+if [ $RET -ne 0 ]; then
+    echo "failed on pushing git"
+    echo "exit value:" $RET
+    exit 1
+fi
